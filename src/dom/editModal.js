f31469534606ccffe1047dfc30bd2f5e63d5cf7e
Josh King <162310653+jjjmdev@users.noreply.github.com>
2024-08-09 12:08:38 +0800

    Code refactor and add functionality to edit modal

--- src/dom/editModal.js
+++ src/dom/editModal.js
@@ -2,152 +2,148 @@ import { itemByIndex } from "../app_logic/logic.js";
 import { format } from "date-fns";
 import "bulma-switch";
 
+function createLabel(text, forAttribute) {
+	const label = document.createElement("label");
+	label.classList.add("label");
+	label.setAttribute("for", forAttribute);
+	label.textContent = text;
+
+	return label;
+}
+
+function createControl() {
+	const control = document.createElement("div");
+	control.classList = "control";
+
+	return control;
+}
+
+function createInput(text, name, value, type, placeholder = "") {
+	const field = createElement("div", "field");
+	const label = createLabel(text, name);
+	const control = createControl();
+
+	const textInput = document.createElement("input");
+	textInput.classList = "input";
+	textInput.setAttribute("type", type);
+	textInput.setAttribute("id", name);
+	textInput.setAttribute("name", name);
+	textInput.setAttribute("value", value);
+	textInput.setAttribute("placeholder", placeholder);
+
+	control.append(label, textInput);
+	field.append(control);
+	return field;
+}
+
+function createSwitch(text, name, checked) {
+	const field = createElement("div", "field");
+	const label = createLabel(text, name);
+	const control = createControl();
+
+	const textInput = document.createElement("input");
+	textInput.classList = "switch";
+	textInput.setAttribute("type", "checkbox");
+	textInput.setAttribute("id", name);
+	textInput.setAttribute("name", name);
+
+	if (checked) {
+		textInput.setAttribute("checked", "checked");
+	}
+
+	control.append(textInput, label);
+	field.append(control);
+	return field;
+}
+
+function createElement(type, classList = "") {
+	const el = document.createElement(type);
+	el.classList = classList;
+
+	return el;
+}
+
+function createSelect(selected, title, name, options) {
+	const field = createElement("div", "field");
+	const label = createLabel(title, name);
+	const control = createControl();
+	const selectContainer = createElement("div", "select");
+	const select = createElement("select");
+	select.setAttribute("id", name);
+	select.setAttribute("name", name);
+
+	select.append(
+		...options.map(({ value, text }) => {
+			const el = document.createElement("option");
+			el.setAttribute("value", value);
+			el.textContent = text;
+
+			if (value === selected) {
+				el.setAttribute("selected", "selected");
+			}
+
+			return el;
+		})
+	);
+	selectContainer.append(select);
+	control.append(label, selectContainer);
+	field.append(control);
+	return field;
+}
+
 export function renderEditModal(index) {
 	const { projectName, title, description, priority, completed, date, id } =
 		itemByIndex(index);
 
 	// Modal
 
-	const modal = document.createElement("div");
-	modal.classList = "modal is-active";
+	const modal = createElement("div", "modal is-active");
+	const modalBackground = createElement("div", "modal-background");
+	const modalCard = createElement("div", "modal-card");
+	const modalCardHead = createElement("header", "modal-card-head");
 
-	const modalBackground = document.createElement("div");
-	modalBackground.classList = "modal-background";
-
-	const modalCard = document.createElement("div");
-	modalCard.classList = "modal-card";
-
-	const modalCardHead = document.createElement("header");
-	modalCardHead.classList = "modal-card-head";
-
-	const modalCardTitle = document.createElement("p");
-	modalCardTitle.classList = "modal-card-title";
+	const modalCardTitle = createElement("p", "modal-card-title");
 	modalCardTitle.textContent = "Edit an item";
+	const modalCardDelete = createElement("button", "delete");
 
-	const modalCardDelete = document.createElement("button");
-	modalCardDelete.classList = "delete";
+	const modalCardForm = createElement("form");
+	const modalCardBody = createElement("section", "modal-card-body");
 
-	const modalCardBody = document.createElement("section");
-	modalCardBody.classList = "modal-card-body";
-
-	const projectNameField = document.createElement("div");
-	projectNameField.classList.add("field");
-
-	const projectNameLabel = document.createElement("label");
-	projectNameLabel.classList.add("label");
-	projectNameLabel.textContent = "Project Name";
-
-	const projectNameControl = document.createElement("div");
-	projectNameControl.classList.add("control");
-
-	const projectNameInput = document.createElement("input");
-	projectNameInput.classList.add("input");
-	projectNameInput.setAttribute("type", "text");
-	projectNameInput.setAttribute("placeholder", "Enter project name");
-	projectNameInput.setAttribute("value", projectName);
-
-	projectNameControl.appendChild(projectNameInput);
-	projectNameField.append(projectNameLabel, projectNameControl);
-
-	const taskField = document.createElement("div");
-	taskField.classList.add("field");
-
-	const taskLabel = document.createElement("label");
-	taskLabel.classList.add("label");
-	taskLabel.textContent = "Task";
-
-	const taskControl = document.createElement("div");
-	taskControl.classList.add("control");
-
-	const taskInput = document.createElement("input");
-	taskInput.classList.add("input");
-	taskInput.setAttribute("type", "text");
-	taskInput.setAttribute("placeholder", "Enter task name");
-	taskInput.setAttribute("value", title);
-
-	taskControl.appendChild(taskInput);
-	taskField.append(taskLabel, taskControl);
-
-	const deadlineField = document.createElement("div");
-	deadlineField.classList = "field cell";
-
-	const deadlineLabel = document.createElement("label");
-	deadlineLabel.classList.add("label");
-	deadlineLabel.textContent = "Deadline";
-
-	const deadlineControl = document.createElement("div");
-	deadlineControl.classList.add("control");
-
-	const deadlineInput = document.createElement("input");
-	deadlineInput.classList.add("input");
-	deadlineInput.setAttribute("type", "date");
-	deadlineInput.setAttribute("value", format(new Date(date), "yyyy-MM-dd"));
-
-	deadlineControl.appendChild(deadlineInput);
-	deadlineField.append(deadlineLabel, deadlineControl);
-
-	const completedField = document.createElement("div");
-	completedField.classList = "field cell";
-
-	const completedLabel = document.createElement("label");
-	completedLabel.classList.add("label");
-	completedLabel.setAttribute("for", "completedSwitch");
-	completedLabel.textContent = "Completed?";
-
-	const completedControl = document.createElement("div");
-	completedControl.classList.add("control");
-
-	const completedInput = document.createElement("input");
-	completedInput.classList = "switch";
-	completedInput.setAttribute("type", "checkbox");
-	completedInput.setAttribute("id", "completedSwitch");
-	completedInput.setAttribute("name", "completedSwitch");
-	completedInput.setAttribute("checked", "checked");
-
-	completedControl.append(completedInput, completedLabel);
-	completedField.append(completedControl);
-
-	const priorityField = document.createElement("div");
-	priorityField.classList = "field cell";
-
-	const priorityLabel = document.createElement("label");
-	priorityLabel.classList.add("label");
-	priorityLabel.textContent = "Priority";
-
-	const priorityControl = document.createElement("div");
-	priorityControl.classList.add("control");
-
-	const prioritySelectContainer = document.createElement("div");
-	prioritySelectContainer.classList.add("select");
-
-	const prioritySelect = document.createElement("select");
-
-	const lowPriorityOption = document.createElement("option");
-	lowPriorityOption.textContent = "Low";
-
-	const mediumPriorityOption = document.createElement("option");
-	mediumPriorityOption.textContent = "Medium";
-
-	const highPriorityOption = document.createElement("option");
-	highPriorityOption.textContent = "High";
-
-	if (priority === 1) {
-		lowPriorityOption.setAttribute("selected", "selected");
-	} else if (priority === 2) {
-		mediumPriorityOption.setAttribute("selected", "selected");
-	} else {
-		highPriorityOption.setAttribute("selected", "selected");
-	}
-
-	prioritySelect.append(
-		lowPriorityOption,
-		mediumPriorityOption,
-		highPriorityOption
+	// Project Name
+	const projectNameField = createInput(
+		"Project Name",
+		"projectName",
+		projectName,
+		"text",
+		"Enter project name"
 	);
 
-	prioritySelectContainer.appendChild(prioritySelect);
-	priorityControl.appendChild(prioritySelectContainer);
-	priorityField.append(priorityLabel, priorityControl);
+	const taskField = createInput(
+		"Task",
+		"title",
+		title,
+		"text",
+		"Enter task name"
+	);
+
+	const deadlineField = createInput(
+		"Deadline",
+		"date",
+		format(new Date(date), "yyyy-MM-dd"),
+		"date"
+	);
+
+	const priorityField = createSelect(priority, "Priority", "priority", [
+		{ value: 1, text: "Low" },
+		{ value: 2, text: "Medium" },
+		{ value: 3, text: "High" },
+	]);
+
+	const completedField = createSwitch(
+		"Completed?",
+		"completedSwitch",
+		completed
+	);
 
 	const formGrid = document.createElement("div");
 	formGrid.classList = "grid";
@@ -160,9 +156,10 @@ export function renderEditModal(index) {
 	const modalFooterButtons = document.createElement("div");
 	modalFooterButtons.classList = "buttons";
 
-	const modalConfirm = document.createElement("button");
+	const modalConfirm = document.createElement("input");
 	modalConfirm.classList = "button is-success";
-	modalConfirm.textContent = "Apply Changes";
+	modalConfirm.setAttribute("type", "submit");
+	modalConfirm.setAttribute("value", "Apply Changes");
 
 	const modalCancel = document.createElement("button");
 	modalCancel.classList = "button";
@@ -172,8 +169,10 @@ export function renderEditModal(index) {
 	modalCardFooter.append(modalFooterButtons);
 
 	modalCardHead.append(modalCardTitle, modalCardDelete);
-	modalCard.append(modalCardHead, modalCardBody, modalCardFooter);
+	modalCardForm.append(modalCardBody, modalCardFooter);
+	modalCard.append(modalCardHead, modalCardForm);
 	modal.append(modalBackground, modalCard);
+
 	document.body.append(modal);
 
 	[modalBackground, modalCardDelete, modalCancel].forEach(function (element) {
@@ -182,10 +181,13 @@ export function renderEditModal(index) {
 		});
 	});
 
-	modalConfirm.addEventListener("click", function () {
-		// call pang delete
-		deleteItemByIndex(index);
-		modal.remove();
-		renderDom();
+	modalCardForm.addEventListener("submit", function (e) {
+		// call pang edit
+		e.preventDefault();
+
+		const formData = new FormData(e.target);
+		const formProps = Object.fromEntries(formData);
+
+		console.log(formProps);
 	});
 }
